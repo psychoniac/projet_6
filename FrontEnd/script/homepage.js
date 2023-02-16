89a4f886f7460f2e4c3f9965fb24d6d12ce9e463
psychoniac <jlnko@hotmail.fr>
2023-02-16 21:24:32 +0100

    rajout de details dans les fonctions creer precedemment, fin de jourée du 16/02

--- FrontEnd/script/homepage.js
+++ FrontEnd/script/homepage.js
@@ -1,23 +1,30 @@
-//Recupere la liste des travaux
-function recuperationListeTravaux () {
-  //Faire un fetch pour recuperer la liste des travaux
-  fetch('http://localhost:5678/api/works')
-    .then(res => res.json())
-    .then(donnees => console.log(donnees))
-}
+window.addEventListener('DOMContentLoaded', () => {
+  //Recupere la liste des travaux
+  async function recuperationListeTravaux () {
+    //Faire un fetch pour recuperer la liste des travaux
+    let reponse = await fetch('http://localhost:5678/api/works')
+    let listeTravaux = await reponse.json()
+    creationBaliseHtml()
+    let image1 = document.getElementById('image1')
+    image1.src = listeTravaux[0].imageUrl
+  }
 
-//on vide la partie du HTML qui contient les travaux
-function remiseZeroGallerie () {
-  document.querySelector('.gallery').innerHTML = ''
-}
-remiseZeroGallerie()
-recuperationListeTravaux()
+  //on vide la partie du HTML qui contient les travaux
+  function remiseZeroGallerie () {
+    document.querySelector('.gallery').innerHTML = ''
+  }
+  remiseZeroGallerie()
 
-function creationBaliseHtml () {
-  let gallerie = document.querySelector('.gallery')
-  let baliseFigure = document.createElement('figure')
-  gallerie.appendChild(baliseFigure)
-  let baliseFigcaption = document.createElement('figcaption')
-  baliseFigure.appendChild(baliseFigcaption)
-}
-creationBaliseHtml()
+  function creationBaliseHtml () {
+    let gallerie = document.querySelector('.gallery')
+    let baliseFigure = document.createElement('figure')
+    gallerie.appendChild(baliseFigure)
+    let baliseImg = document.createElement('img')
+    baliseFigure.appendChild(baliseImg)
+    baliseImg.setAttribute('id', 'image1')
+    let baliseFigcaption = document.createElement('figcaption')
+    baliseFigure.appendChild(baliseFigcaption)
+  }
+  creationBaliseHtml()
+  recuperationListeTravaux()
+})
