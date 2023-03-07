9fc19af757b3da5f7941bb06d022e29f83973440
psychoniac <jlnko@hotmail.fr>
2023-03-07 16:19:53 +0100

    fonction login creer

--- FrontEnd/script/login.js
+++ FrontEnd/script/login.js
@@ -1,18 +1,43 @@
 window.addEventListener('DOMContentLoaded', function () {
-  envoiLogin()
+  authentification()
 })
-
-function envoiLogin () {
-  const login = document.querySelector('#mail').value
-  const password = document.querySelector('#password').value
-  fetch('http://localhost/api/users/login', {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'apllication/json'
-    },
-    method: 'POST',
-    body: JSON.stringify({ email: login, password: password })
-  }).then(res => {
-    console.log(res)
+function authentification () {
+  const formulaireLogin = document.querySelector('#loginForm')
+  formulaireLogin.addEventListener('submit', function (e) {
+    e.preventDefault()
+    requetePostLogin()
   })
+}
+
+async function requetePostLogin () {
+  const formulaireData = {
+    email: document.querySelector('#mail').value,
+    password: document.querySelector('#password').value
+  }
+
+  const idUtilisateur = JSON.stringify(formulaireData)
+  let token = ''
+  const reponse = await fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`
+    },
+    body: idUtilisateur
+  })
+  if (reponse.status === 200) {
+    console.log('response ok')
+    let resultat = await reponse.json()
+    let userToken = JSON.stringify(resultat)
+    localStorage.setItem('token', `${userToken}`)
+    // document.location.href = './index.html'
+    console.log(localStorage)
+  } else {
+    erreurMessage(reponse)
+  }
+}
+function erreurMessage (reponseRequete) {
+  if (reponseRequete.status != 200) {
+    alert(reponseRequete.status)
+  }
 }
